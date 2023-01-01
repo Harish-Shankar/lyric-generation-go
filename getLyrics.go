0c5421205a7bb75d99d3628e90d8530c99321162
Harish Shankar <harish@Harishs-MacBook-Pro.local>
2023-01-01 19:10:18 +0530

    Setting Up Connection With Spotify Client

--- getLyrics.go
+++ getLyrics.go
@@ -1,1 +1,36 @@
 package main
+
+import (
+	"context"
+	"log"
+	"os"
+
+	"github.com/joho/godotenv"
+	"github.com/zmb3/spotify/v2"
+	spotifyauth "github.com/zmb3/spotify/v2/auth"
+	"golang.org/x/oauth2/clientcredentials"
+)
+
+func getLyrics() {
+	var err = godotenv.Load(".env")
+	if err != nil {
+		panic(err)
+	}
+	var CLIENT_ID = os.Getenv("CLIENT_ID")
+	var CLIENT_SECRET = os.Getenv("CLIENT_SECRET")
+
+	var ctx = context.Background()
+	var config = &clientcredentials.Config{
+		ClientID:     CLIENT_ID,
+		ClientSecret: CLIENT_SECRET,
+		TokenURL:     spotifyauth.TokenURL,
+	}
+
+	token, err := config.Token(ctx)
+	if err != nil {
+		log.Fatalf("couldn't get token: %v", err)
+	}
+
+	httpClient := spotifyauth.New().Client(ctx, token)
+	client := spotify.New(httpClient)
+}
--- go.mod
+++ go.mod
@@ -1,3 +1,16 @@
 module lyric-generation-go
 
 go 1.19
+
+require (
+	github.com/joho/godotenv v1.4.0
+	github.com/zmb3/spotify/v2 v2.3.0
+	golang.org/x/oauth2 v0.3.0
+)
+
+require (
+	github.com/golang/protobuf v1.5.2 // indirect
+	golang.org/x/net v0.3.0 // indirect
+	google.golang.org/appengine v1.6.7 // indirect
+	google.golang.org/protobuf v1.28.0 // indirect
+)
