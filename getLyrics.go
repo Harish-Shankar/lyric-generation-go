63652c285eca9126af4735cb6f805127cd3a854d
Harish Shankar <harish@Harishs-MacBook-Pro.local>
2023-01-01 19:11:47 +0530

    Retrieve All Albums Made By Specified Artist

--- getLyrics.go
+++ getLyrics.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"context"
+	"fmt"
 	"log"
 	"os"
 
@@ -10,6 +11,39 @@ import (
 	spotifyauth "github.com/zmb3/spotify/v2/auth"
 	"golang.org/x/oauth2/clientcredentials"
 )
+
+func getAllAlbums(ctx context.Context, artist string, client spotify.Client) []spotify.ID {
+	results, err := client.Search(ctx, artist, spotify.SearchTypeArtist)
+	if err != nil {
+		panic(err)
+	}
+	var artist_id spotify.ID
+	if results.Artists != nil {
+		for _, item := range results.Artists.Artists {
+			if item.Name == artist {
+				artist_id = item.ID
+				break
+			}
+		}
+	}
+
+	artist_albums, err := client.GetArtistAlbums(ctx, artist_id, []spotify.AlbumType{1}, spotify.Market("US") /* , spotify.Limit(50) */)
+	if err != nil {
+		panic(err)
+	}
+	var album_ids []spotify.ID
+	var album_names []string
+	for _, album := range artist_albums.Albums {
+		album_ids = append(album_ids, album.ID)
+		album_names = append(album_names, album.Name)
+	}
+
+	for i := range album_names {
+		fmt.Println(album_names[i])
+	}
+
+	return album_ids
+}
 
 func getLyrics() {
 	var err = godotenv.Load(".env")
@@ -33,4 +67,7 @@ func getLyrics() {
 
 	httpClient := spotifyauth.New().Client(ctx, token)
 	client := spotify.New(httpClient)
+
+	artist := "Kendrick Lamar"
+	albumIDList := getAllAlbums(ctx, artist, *client)
 }
